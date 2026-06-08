6483af321c8e3f5d0497fd3a246f8a62f1b3697b
Rafael <rafadz102@gmail.com>
2026-06-08 12:08:54 -0500

    Correción en el título

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let numeroMaximo = 60;
 
 
 function condicionesIniciales(){
-    asginarTextoElemento('h1','Juego de los números secretos! '); 
+    asginarTextoElemento('h1','Juego del número secreto! '); 
 }
 
 
